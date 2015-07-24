digint/btrfs-progs

commit dedb1ebeee847e3c4d71e14d0c1077887630e44a

btrfs-progs: defrag, check target extent earlier
Print a warning if the target extent size (option -t) is larger than 4G.

Signed-off-by: David Sterba <dsterba@suse.cz>

cmds-filesystem.c
@@ -1170,6 +1170,11 @@ static int cmd_defrag(int argc, char **argv)
 			break;
 		case 't':
 			thresh = parse_size(optarg);
+			if (thresh > (u32)-1) {
+				fprintf(stderr,
+			"WARNING: target extent size %llu too big, trimmed to %u",
+					thresh, (u32)-1);
+			}
 			defrag_global_fancy_ioctl = 1;
 			break;
 		case 'r':
@@ -1186,7 +1191,7 @@ static int cmd_defrag(int argc, char **argv)
 	memset(&defrag_global_range, 0, sizeof(range));
 	defrag_global_range.start = start;
 	defrag_global_range.len = len;
-	defrag_global_range.extent_thresh = thresh > (u32)-1 ? (u32)-1 : (u32)thresh;
+	defrag_global_range.extent_thresh = (u32)thresh;
 	if (compress_type) {
 		defrag_global_range.flags |= BTRFS_DEFRAG_RANGE_COMPRESS;
 		defrag_global_range.compress_type = compress_type;

commit f77d87c154cdc82428261ec4fbb10283a9f9f05a

btrfs-progs: defrag: fix threshold overflow again
Commit dedb1ebeee847e3c4d71e14d0c1077887630e44a broke commit
96cfbbf0ea9fce7ecaa9e03964474f407f6e76ab.

Casting thresh value greater than (u32)-1 simply truncates bits while
desired value is (u32)-1 for max defrag threshold.

I.e. "btrfs fi defrag -t 4g" is trimmed/truncated to 0
and "-t 5g" to 1073741824.

Also added a missing newline.

Signed-off-by: Patrik Lundquist <patrik.lundquist@gmail.com>
Signed-off-by: David Sterba <dsterba@suse.com>

cmds-filesystem.c
@@ -1172,8 +1172,9 @@ static int cmd_filesystem_defrag(int argc, char **argv)
 			thresh = parse_size(optarg);
 			if (thresh > (u32)-1) {
 				fprintf(stderr,
-			"WARNING: target extent size %llu too big, trimmed to %u",
+			"WARNING: target extent size %llu too big, trimmed to %u\n",
 					thresh, (u32)-1);
+				thresh = (u32)-1;
 			}
 			defrag_global_fancy_ioctl = 1;
 			break;